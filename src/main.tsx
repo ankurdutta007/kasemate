6665a36c391118f05aef9fc29899f8a0c9728c80
Ankur Dutta <ankurdutta@Ankurs-MacBook-Air-2.local>
2026-08-24 22:04:59 +0530

    Add PostHog analytics integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import posthog from 'posthog-js'
+import { PostHogProvider } from '@posthog/react'
+
+posthog.init(import.meta.env.VITE_POSTHOG_PROJECT_TOKEN, {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider client={posthog}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 )
