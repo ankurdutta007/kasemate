35e02c058f9b42aeb7e83a7dab64d0dd2e21c7db
Ankur Dutta <ankurdutta@Ankurs-MacBook-Air-2.local>
2026-08-24 20:02:30 +0530

    Fix missing Analytics import causing runtime crash

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from './context/ThemeContext'
 import { CasesProvider } from './context/CasesContext'
 import Nav from './components/Nav'
